ba9996605a8da509ed1321ab39277d135a97e0dd
Belbut <80793911+Belbut@users.noreply.github.com>
2024-05-13 17:52:12 +0100

    Define HashMap, set function and intuitive display

---
+++ hash_map.rb
@@ -0,0 +1,75 @@
+class HashMap
+  attr_accessor :buckets
+
+  def initialize
+    @buckets = Array.new(16) { LinkedList.new }
+  end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+    hash_code
+  end
+
+  def set(key, value)
+    hash_code = hash(key)
+    index = hash_code % 15
+    raise IndexError if index.negative? || index >= buckets.length
+
+    buckets[index].set(key, value)
+  end
+
+  def to_s
+    result = "-----------------------------------\n"
+    buckets.each_with_index do |bucket, index|
+      result << "[B#{index.to_s.rjust(2, '0')}]-List:#{bucket}"[0..-3]
+      result << "\n"
+    end
+
+    result
+  end
+end
+
+class LinkedList
+    attr_accessor :next_node, :key , :value
+  def initialize(key = nil, value = nil, next_node = nil)
+    @key = key
+    @value = value
+    @next_node = next_node
+  end
+
+  def set(key, value)
+    slack_node = node = self
+
+    until node.next_node.nil? || node.key == key
+      slack_node = node
+      node = node.next_node
+    end
+
+    if node.key == key
+      slack_node.next_node = Node.new(key, value, node.next_node)
+    else
+      node.next_node = Node.new(key, value)
+    end
+  end
+
+  def to_s
+    "HEAD=>  #{@next_node}"
+  end
+end
+
+class Node
+  attr_accessor :key, :value, :next_node
+
+  def initialize(key = nil, value = nil, next_node = nil)
+    @key = key
+    @value = value
+    @next_node = next_node
+  end
+
+  def to_s
+    "(#{key}:#{value})->#{next_node}"
+  end
+end
---
+++ test.rb
@@ -0,0 +1,7 @@
+require './hash_map.rb'
+hm = HashMap.new
+hm.set("Carla",27)
+hm.set("Carlos",28)
+hm.set("Carla",22)
+
+puts hm
